7aaca92e3efc18b2c208b07b4bc7b7c9d185df5d
parkjinhwa <parkjinhwa3914@gmail.com>
2022-05-08 13:21:38 +0900

    modified things

--- backend/file_process/main.js
+++ backend/file_process/main.js
@@ -1,29 +1,43 @@
-var express = require('express')
-var app = express();
-var port = 5000;
-/* module */
-var multer = require('multer')
-const _storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, './upload')
-    },
-    filename: function (req, file, cb) {
-        cb(null, file.originalname)
+const multer = require("multer");
+
+const handleError = (err, res) => {
+    res
+        .status(500)
+        .contentType("text/plain")
+        .end("Oops! Something went wrong!");
+};
+
+const upload = multer({
+    dest: "/path/to/temporary/directory/to/store/uploaded/files"
+    // you might also want to set some limits: https://github.com/expressjs/multer#limits
+});
+
+
+app.post(
+    "/upload",
+    upload.single("file" /* name attribute of <file> element in your form */),
+    (req, res) => {
+        const tempPath = req.file.path;
+        const targetPath = path.join(__dirname, "./uploads/image.png");
+
+        if (path.extname(req.file.originalname).toLowerCase() === ".png") {
+            fs.rename(tempPath, targetPath, err => {
+                if (err) return handleError(err, res);
+
+                res
+                    .status(200)
+                    .contentType("text/plain")
+                    .end("File uploaded!");
+            });
+        } else {
+            fs.unlink(tempPath, err => {
+                if (err) return handleError(err, res);
+
+                res
+                    .status(403)
+                    .contentType("text/plain")
+                    .end("Only .png files are allowed!");
+            });
+        }
     }
-})
-var upload = multer({ storage: _storage })
-
-app.use(express.static('public'))
-
-app.get('/', (req, res) => {
-    res.sendFile('/')
-})
-
-app.post('/uploadDo', upload.any(), (req, res) => {
-    console.log(req.files)
-    res.send('Upload file = ' + req.files)
-})
-
-app.listen(port, () => {
-    console.log(`http://localhost:${port}`)
-})
+);
